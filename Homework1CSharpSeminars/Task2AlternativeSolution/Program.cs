04dc4568e8aaff2eb46c13a86b78f2be86f4520d
iStoik <vuoksa@bk.ru>
2022-07-15 01:19:21 +0300

    Save job seminar #1

---
+++ Homework1CSharpSeminars/Task2AlternativeSolution/Program.cs
@@ -0,0 +1,20 @@
+﻿int [] numbers = new int[3];
+int i = 0;
+int max = 0;
+Console.WriteLine("Введите три числа, и узнайте, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число и нажмите ENTER: ");
+numbers[0] = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число и нажмите ENTER: ");
+numbers[1] = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число и нажмите ENTER: ");
+numbers[2] = int.Parse(Console.ReadLine());
+while (i < numbers.Length)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    i++;
+}
+Console.Write("Максимальное значение имеет число " + max);
---
+++ Homework1CSharpSeminars/Task4AlternativeSolution/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int rem = 1;
+while (rem <= num)
+{
+    if (rem % 2 == 0)
+    {
+        Console.Write(rem + " ");
+    }
+    rem++;
+}
